6c0a725bf898aeaf9ca5225b840bb2e2917aa649
Daniel Nyong <danielnyong66@gmail.com>
2020-12-24 19:17:05 -0500

    modified:   TextBox.js

--- src/TextBox.js
+++ src/TextBox.js
@@ -5,28 +5,48 @@ class TextBox extends React.Component
   constructor(props) {
     super(props);
     this.state = {
-      line: "",
-      nextline: {}
+        line: [''],
+        index: 0,
+        nextline: {}
     } 
+    this.string = '';
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
-  
+ /* 
+  print2Screen(state) {
+    output = '';
+    while (state.nextline === undefined) 
+    {
+        while (state.line['0'] !== undefined) {
+            output += state.line['0'];
+        }
+        state = state.nextline;
+    }
+  }
+*/
   handleKeyPress(e) {
     if (e.key === "Backspace") 
         this.setState(state => ({
             line: state.line.substring(0, state.line.length-1)
         }))
     else if (e.keyCode === 32)
-        this.setState(
-                state => ({line: state.line+ '\x20\x20\u200C\x20\x20\u200C'})
-            )
+        this.setState(state => {
+                    this.string += '\x20\x20\u200C';
+                    state.index++;
+                    let retVal = {line: state.line[state.index] + }
+                    return retVal;
+                })
     else{
         if (e.key === "Shift" || e.key === "CapsLock") 
             return
         else {
-            console.log(this.state.line);
+            console.log(this.state);
+            this.string+= e.key
             this.setState(
-                state => ({line: state.line+ e.key})
+                state =>  {
+
+                    ({line: [this.string]})
+                }
             )
         }
     }
