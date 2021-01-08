525c557d4824767441de63f2b13794b0754f2891
Daniel Nyong <danielnyong66@gmail.com>
2021-01-08 15:27:40 -0500

    Implemented A caret

--- src/KeyFunctions.js
+++ src/KeyFunctions.js
@@ -1,26 +1,103 @@
-export function Backspace(obj) {
-    let {
-        word: newWord,
-        line: newLine, 
-        index: newIdx
-    } = obj;
+//////////////////////////////////////////////////////////////////////
+//////////       This file handles all Keyboard operations //////////
+/////////////////////////////////////////////////////////////////////
 
-    if (newWord.length <= 1) {
-        if (newLine.length <= 1)
-            newLine = [""]
+const cpyMatrix = array => array.map(elem => [...elem])
+
+export default function handleKeyPress 
+    (e, line, setLine, lIdx, setLIdx, wIdx, setWIdx)  
+{
+    try {
+        if (e.key === "Backspace")
+        {   
+            // Calls the Function that handles backspaces 
+            
+            let {           // Declare new state values...
+                nLine,
+                nLIdx,
+                nWIdx
+            } = Backspace({   /// Modify state values...
+                line: cpyMatrix(line),
+                lIdx,
+                wIdx
+            })
+            /// Update state values...
+            setLine(nLine)
+            setLIdx(nLIdx)
+            setWIdx(nWIdx)
+        }
+        else if (e.keyCode === 32)
+        {
+            /// Modify state values...
+            const {nLine, nLIdx, nWIdx} = spaceBar(e, {line: cpyMatrix(line), lIdx, wIdx})
+            /// Update state values...
+            setLine(nLine)
+            setLIdx(nLIdx)
+            setWIdx(nWIdx)
+        }
         else {
-            newLine.pop()
-            if (newIdx > 0) newIdx-- 
-            newWord = newLine[newIdx]
+            /// Modify state values...
+            const {nLine, nLIdx, nWIdx} = updateLine(e, cpyMatrix(line), lIdx, wIdx)
+            /// Update state values...
+            setLine(nLine)
+            setLIdx(nLIdx)
+            setWIdx(nWIdx)
+            console.log(line, lIdx, wIdx)
         }
     }
-    else {
-        newWord = newWord.substring(0, newWord.length-1)
-        newLine[newIdx] = newWord
+    catch (e) {
+        console.info(e)
+        console.log(line, lIdx, wIdx)
+    }
+}
+
+function spaceBar(e, {line, lIdx, wIdx}) {
+    e.preventDefault()
+    line[lIdx].push('\x20\u200c')
+    return {
+        nLine: line,
+        nLIdx: lIdx,
+        nWIdx: wIdx + 1
+    }
+}
+
+function updateLine(e, line, lIdx, wIdx) {
+    e.preventDefault()
+    if (e.key.length === 1) 
+    {
+        line[lIdx].push(e.key)
+        wIdx++
+        console.log(wIdx)
     }
     return {
-        newWord,
-        newLine,
-        newIdx
+        nLine: line,
+        nLIdx: lIdx,
+        nWIdx: wIdx 
+    }
+}
+
+function Backspace(obj) {
+    let {
+        line, 
+        lIdx,
+        wIdx
+    } = obj;
+
+    if (line.length <= 1 && line[lIdx].length <= 1)
+    {
+        line[0] = [""]
+        lIdx = 0
+    }
+    else {
+        line[lIdx].pop()
+        if (wIdx > 0) 
+            wIdx-- 
+        else
+            lIdx--
+    }
+    return {
+        nLine: line,
+        nLIdx: lIdx,
+        nWIdx: wIdx
     }
 }
--- src/TextBox.js
+++ src/TextBox.js
@@ -1,78 +1,68 @@
-import React, { useState } from 'react';            
-import {Backspace} from './KeyFunctions.js'
+//////////////////////////////////////////////////////////////////////////////////////////////////
+////////    In This File I Implement A Text box that handles the User's mouse and  /////////////// 
+////////    ...keyboard input and displays the data received on the Screen.    ///////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//eslint-disable-next-line
+import React, { useState, useRef, useEffect } from 'react'            
+import handleKeyPress from './KeyFunctions.js'
 
 function TextBox()
 {
-    const [line, setLine] = useState([''])
-    const [index, setIndex] = useState(0)
-    //const [nextLine, setNextLine] = useState({})
+    /// Manages the state of the lines on the screen
+    const [line, setLine] = useState([['']])
+    /// Line Index...
+    const [lIdx, setLIdx] = useState(0)
+    /// Word Index...
+    const [wIdx, setWIdx] = useState(0)
+   
+    const textRef = useRef()
 
- /* 
-  printScreen(state) {
-    output = '';
-    while (state.nextline === undefined) 
-    {
-        while (state.line['0'] !== undefined) {
-            output += state.line['0'];
-        }
-        state = state.nextline;
+    const cursorRef = useRef()
+
+    const Cursor = () => {
+        return <i id='cursor' ref = {cursorRef}></i>
     }
-  }
-*/
-    const printLine = array => {
+
+    const printCaret = (array, curIdx) => {
+        const displayedText  = [...array]
+        displayedText.splice(curIdx+1,0, <Cursor/>)
+        return displayedText 
+    }
+   
+    const printPage = (line, lIdx, wIdx) => {
         return (
-            array.map((elem,index) => (
-                <span key = {index.toString()}>
-                    {elem + '\u200C'}
-                </span>
-            ))
+            line.map((elem, index) => {
+                if (index == lIdx) 
+                   elem = printCaret(line[lIdx], wIdx)
+                return (
+                    <span className='Text' id='normal-text' 
+                        key={elem[lIdx][wIdx]+lIdx+wIdx}>
+                        {elem}
+                    </span>
+                )
+            })
         )
     }
 
-    const handleKeyPress = (e) => {
-    try {
-        /** Create function files that handle each keyboard operation **/
-        if (e.key === "Backspace")
-        {   /** Calls the Function that handles backspaces **/
-            let {
-                newLine,
-                newIdx, 
-            } = Backspace({
-                word: line[index],
-                line: [...line],
-                index
-            })
-            setIndex(newIdx)
-            setLine(newLine)
-        }
-        else if (e.keyCode === 32)
-        {
-            e.preventDefault()
-            let updateLine = line
-            updateLine.push('\x20')
-            let newIndex = updateLine.length
-            updateLine[newIndex] = ''
-            setIndex(newIndex)
-            setLine([...updateLine])
-        }
-        else
-        {
-            if (e.key === "Shift" || e.key === "CapsLock") 
-                return
-            else {
-                let updateLine = line
-                updateLine[index] += e.key
-                setLine([...updateLine])
-            }
-        }
-    }
-    catch (e) {
-        console.log(line.length-1, index, e)
-    }
- }
     return (
-        <div id="txtbox" tabIndex="0" onKeyDown={handleKeyPress}>
-            <p>{printLine(line)}</p>
+        <div id="txtbox" 
+            tabIndex="0" 
+            onKeyDown={(e) => handleKeyPress(
+                e,
+                line, 
+                setLine, 
+                lIdx,
+                setLIdx,
+                wIdx,
+                setWIdx,
+                textRef
+            )}>
+                <p>
+                    {printPage(line, lIdx, wIdx)}
+                </p>
         </div>
     )
 }
