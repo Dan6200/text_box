9aa8d17478beed663ac2c71ce95f3d9ee9a17a97
Dan6200 <danielnyong66@gmail.com>
2021-02-18 18:53:58 -0500

    set up tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,38 @@
 import React from "react";
 import { render, screen } from '@testing-library/react';
-import { render, unmountComponentAtNode } from "react-dom";
+import { unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import App from './App';
+import modifier from './Reducer.js';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('adds letter to keyboard state', () => {
+	expect(modifier(
+	{
+		line: [[]], 
+		lIdx: 0,
+		wIdx: 0,
+	},
+		{type: 'A'}
+	).line[0][0]).toBe('A')
+})
+
+test('Left Scrolling', () => {
+	expect((() => {
+		const {lIdx, wIdx} = modifier({lIdx: 0, wIdx: 1,}, {type: "ArrowLeft"})
+		return {lIdx, wIdx}
+	})()).toEqual({lIdx: 0, wIdx: 0})
+
+	expect((() => {
+		const {lIdx, wIdx} = modifier({lIdx: 0, wIdx: 0,}, {type: "ArrowLeft"})
+		return {lIdx, wIdx}
+	})()).toEqual({lIdx: 0, wIdx: 0})
+
+	expect((() => {
+		const {lIdx, wIdx} = modifier({line: [['A', 'B'], ['C']], 
+			lIdx: 1, wIdx: 0,}, {type: "ArrowLeft"})
+		return {lIdx, wIdx}
+	})()).toEqual({lIdx: 0, wIdx: 2})
+})
+
+
+
